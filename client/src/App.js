55b2ce942f5b46f2b0e47db573dd25bd6a6a3b6e
Unknown <humberto.soares@gmail.com>
2019-10-17 16:57:20 -0300

    Inicio Camera

--- client/src/App.js
+++ client/src/App.js
@@ -1,74 +1,41 @@
 import React, { Component } from 'react';
-
 import logo from './logo.svg';
-
 import './App.css';
+import Notifier from './components/Notifier';
 
 class App extends Component {
-  state = {
-    response: '',
-    post: '',
-    responseToPost: '',
-  };
-
-  componentDidMount() {
-    this.callApi()
-    .then(res => this.setState({ response: res.express }))
-    .catch(err => console.log(err));
+  constructor() {
+    super();
+    this.state = {
+      offline: false
+    }
   }
 
-  callApi = async () => {
-    const response = await fetch('/api/hello');
-    const body = await response.json();
-    if (response.status !== 200) throw Error(body.message);
-
-    return body;
-  };
-
-  handleSubmit = async e => {
-    e.preventDefault();
-    const response = await fetch('/api/world', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ post: this.state.post }),
+  componentDidMount() {
+    window.addEventListener('online', () => {
+      this.setState({ offline: false });
     });
-    const body = await response.text();
 
-    this.setState({ responseToPost: body });
-  };
+    window.addEventListener('offline', () => {
+      this.setState({ offline: true });
+    });
+  }
+
+  componentDidUpdate() {
+    let offlineStatus = !navigator.onLine;
+    if (this.state.offline !== offlineStatus) {
+      this.setState({ offline: offlineStatus });
+    }
+  }
 
   render() {
     return (
         <div className="App">
+          <Notifier offline={this.state.offline} />
           <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-              Edit <code>src/App.js</code> and save to reload.
-            </p>
-            <a
-                className="App-link"
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
+            <img src={logo} className="App-logo" alt="Cloudinary Logo" />
+            <h1 className="App-title">CloudyCam</h1>
           </header>
-          <p>{this.state.response}</p>
-          <form onSubmit={this.handleSubmit}>
-            <p>
-              <strong>Post to Server:</strong>
-            </p>
-            <input
-                type="text"
-                value={this.state.post}
-                onChange={e => this.setState({ post: e.target.value })}
-            />
-            <button type="submit">Submit</button>
-          </form>
-          <p>{this.state.responseToPost}</p>
         </div>
     );
   }
---
+++ client/src/components/Notifier/index.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import "./Notifier.css";
+import classnames from 'classnames';
+
+class Notifier extends Component {
+  render() {
+    const notifyclass = classnames('notify', {
+      danger: this.props.offline
+    });
+    const message = this.props.offline ?
+        `CloudyCam is offline! Your images will be saved now and then uploaded to your Cloudinary Media Library once your Internet connection is back up.`
+        :
+        `Take a picture and it will be uploaded to your Cloudinary Media Library.`;
+    return (
+        <div className={notifyclass}>
+          <p>
+            <em>{message}</em>
+          </p>
+        </div>
+    );
+  }
+}
+
+export default Notifier;
